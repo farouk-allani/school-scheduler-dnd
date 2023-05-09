0cfbc4cc0ded33182d818a39b76b5c2b9af7ba2b
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2023-05-09 09:21:17 +0100

    feat: add kesm component

---
+++ components/Kesm.js
@@ -0,0 +1,36 @@
+import { useDrag } from "react-dnd";
+import React from "react";
+
+export const kesmTypes = {
+  KESM: "kesm",
+};
+
+const Subject = ({ id, name }) => {
+ 
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: kesmTypes.KESM,
+    item: { id, name },
+
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
+  return (
+    <div
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        border: "solid 2px",
+        width: "fit-content",
+        cursor: "move",
+        padding: "5px",
+        margin: "5px",
+      }}
+    >
+      {name}
+    </div>
+  );
+};
+
+export default Subject;
